ae0bafb5b76d9eb0927b133360f93478ffbb9d86
juliet-wambua <julienea289@gmail.com>
2019-07-08 15:10:32 +0300

    Add highlight-directive

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,14 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import {FormsModule} from '@angular/forms'
+
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { QuoteComponent } from './quote/quote.component';
 import { FormComponent } from './form/form.component';
 import { DetailsComponent } from './details/details.component';
+import { HighlightDirective } from './highlight-directive';
 
 @NgModule({
   declarations: [
